dd81b3e16916603c1e7a03f118f2046e92d148d9
voodee <eedoov@gmail.com>
2019-03-16 15:26:08 +0300

    fix memory leak

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let lastResult = ["..."];
 (async () => {
   try {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"]
     });
     const page = await browser.newPage();
