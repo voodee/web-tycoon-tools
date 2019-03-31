aff645590976acfc59e86da781ce78aab6ba7508
voodee <eedoov@gmail.com>
2019-03-31 16:29:49 +0300

    fix 4 heroku

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ const app = express();
 
 let lastResult = ["..."];
 
+const addLog = (type, text) => {
+  console[type](text);
+
+  lastResult.push(
+    `[${new Date(Date.now()).toLocaleString()}] ${type}: ${text}`
+  );
+  if (lastResult.length > 200) {
+    lastResult.shift();
+  }
+};
+
+const logger = {
+  log: addLog.bind(null, "log"),
+  info: addLog.bind(null, "info"),
+  warn: addLog.bind(null, "warn"),
+  error: addLog.bind(null, "error")
+};
+
 (async () => {
   let config = {};
   config.userAgent = ua.chrome(72);
@@ -34,8 +52,8 @@ let lastResult = ["..."];
     };
     try {
       await Promise.all([
-        tasks(browser, console, config),
-        workers(browser, console, config)
+        tasks(browser, logger, config),
+        workers(browser, logger, config)
         // spam(console, config)
       ]);
     } catch (e) {
@@ -45,10 +63,16 @@ let lastResult = ["..."];
   }
 })();
 
-// app.get("/", function(req, res) {
-//   res.send(`<pre>${lastResult.join("\n")}</pre>`);
-// });
+app.get("/", function(req, res) {
+  res.send(
+    `<pre>${lastResult
+      .slice()
+      .reverse()
+      .join("\n")}</pre>
+      <script>setTimeout(location.reload.bind(location), 5000)</script>`
+  );
+});
 
-// app.listen(process.env.PORT || 8080, function() {
-//   console.log("App listening");
-// });
+app.listen(process.env.PORT || 8080, function() {
+  console.log("App listening on port ", process.env.PORT);
+});
