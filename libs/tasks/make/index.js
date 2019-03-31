1488dab95ccfc4bac7baa56683d0718c71c1f491
voodee <eedoov@gmail.com>
2019-03-31 18:48:00 +0300

    fix memory leak

--- libs/tasks/make/index.js
+++ libs/tasks/make/index.js
@@ -141,7 +141,7 @@ module.exports = async (page, logger, config) => {
     } catch (e) {
       logger.error(`Ошибка управления сайтом`, e);
     }
-    if ((siteNumber + 1) % 20 === 0) await page.reload();
+    if (siteNumber % 10 === 0) await page.reload();
   }
 
   logger.info(`Задача по управлению задачами закончена`);
