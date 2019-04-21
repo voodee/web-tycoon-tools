11f0ce642e912c586a17b360ff4253647271d87d
voodee <eedoov@gmail.com>
2019-04-21 04:18:40 +0300

    fix ads

--- libs/tasks/make/content.js
+++ libs/tasks/make/content.js
@@ -17,7 +17,13 @@ module.exports = async (page, logger) => {
    **/
   logger.info(`Смотрим контент`);
   // готовый контент
-  const $goodContent = await page.$$(".contentTypesWr .item .cardContainer");
+  let $goodContent = 0;
+  let numberTry = 0;
+  do {
+    await new Promise(res => setTimeout(res, 1000));
+    $goodContent = await page.$$(".contentTypesWr .item .cardContainer");
+    ++numberTry;
+  } while ($goodContent.length < 4 || numberTry > 10);
 
   const $taskMarketing = await page.$(".taskItem.marketing");
   const $cancelTaskMarketingButton = await $taskMarketing.$(".cancelTask");
@@ -48,6 +54,9 @@ module.exports = async (page, logger) => {
   ) {
     // то публикуем
 
+    await page.click(".panelWrapper .tabWrapper .tab:nth-child(1)");
+    await page.click(".externalLinkWr .baseButton:first-child");
+
     // читаем комментарии
     const comments = await page.evaluate(() => {
       return [
@@ -64,6 +73,7 @@ module.exports = async (page, logger) => {
       acc.push([type, countType]);
       return acc;
     }, []);
+
     const contentCostKeySorted = contentCost
       .sort((a, b) => a[1] - b[1])
       .reverse();
@@ -90,11 +100,7 @@ module.exports = async (page, logger) => {
       $content4publish = $goodContent[contentIndex2];
     }
 
-    logger.info("namesContent", namesContent);
-    logger.info("contentIndex1", contentIndex1);
-    logger.info("contentIndex2", contentIndex2);
-
-    // await page.evaluate(el => el.click(), $content4publish);
+    await page.evaluate(el => el.click(), $content4publish);
     logger.info("Опубликован контент");
   }
 };
